ae9884f0d6c6095e67cd8e780e029373860ecb2b
brenoam <brenoam@dcc.ufmg.br>
2019-04-30 01:20:02 -0300

    Read input

---
+++ src/simplex.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import logging
+from linearProgramming import LinearProgramming
+
+class Simplex():
+		
+	def run(self):
+		logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(levelname)s\n%(message)s')
+		logger = logging.getLogger(__name__)
+		
+		rows, columns = map(int,input().strip().split(" "))
+		c = list(map(int, input().split()))
+		Ab = [list(map(int, input().split())) for row in range(rows)]
+		
+		logger.info("%i rows", rows)
+		logger.info("%i columns", columns)
+		logger.info(c)
+		logger.info(Ab)
+		
+		lp = LinearProgramming(rows, columns, c, Ab)
+
+if (__name__ == '__main__'):
+		simplex = Simplex()
+		simplex.run()
